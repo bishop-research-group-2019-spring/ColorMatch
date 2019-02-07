3575e400bd581e39922d322a8178837188e6fde8
WanyiChen <wanyi.chen@swim.it>
2019-02-06 21:41:26 -0500

    add touch

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let ids = ["blue", "purple", "green", "orange"];
 let colors = ["#8ccdfc", "#9e5afb", "#c2fb73", "#fa8f41"];
 
 $(document).ready(function() {
-  $(document).mousemove(function(e) {
+  $(document).on('mousemove touchmove', function(e) {
     let curId = $(e.target).attr('id');
     if (curId == $("#header").data("color")) {
       let randomNum = Math.floor(Math.random() * 4); //generate a random number between 0 and 3
