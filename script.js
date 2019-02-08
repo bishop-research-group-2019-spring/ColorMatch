97bb9885a248ed0e1f21dd36ab737c915f82c83b
WanyiChen <wanyi.chen@swim.it>
2019-02-07 23:55:37 -0500

    fix touch

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ let ids = ["blue", "purple", "green", "orange"];
 let colors = ["#8ccdfc", "#9e5afb", "#c2fb73", "#fa8f41"];
 
 $(document).ready(function() {
-  $(document).on('mousemove touchmove', function(e) {
-    let curId = $(e.target).attr('id');
-    if (curId == $("#header").data("color")) {
+
+  $(document).on('touchmove', function(e) {
+    var touch = e.touches[0];
+    let curColor = $("#header").data("color"); //the current header color
+    let targetKey = $("#" + curColor); //the correct key
+
+    //getting the correct key's position
+    let left = $(targetKey).position().left,
+      top = $(targetKey).position().top;
+    let right = left + $(targetKey).width(),
+      bottom = top + $(targetKey).height();
+
+    //if the user touches the correct key, change header
+    if (touch.pageX > left && touch.pageX < right && touch.pageY > top && touch.pageY < bottom) {
       let randomNum = Math.floor(Math.random() * 4); //generate a random number between 0 and 3
       $("#header").data("color", ids[randomNum]);
       $("#header").css("background-color", colors[randomNum]);
     }
-    //console.log($(e.target).attr('id'));
   });
+
 });
